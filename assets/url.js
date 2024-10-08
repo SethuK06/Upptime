8de28bffd537c7f355bcfde2876bbe1f24406fa5
SethuK-ND <sethu@navadhiti.com>
2024-10-08 18:55:57 +0530

    base js file

--- assets/url.js
+++ assets/url.js
@@ -1,15 +1,3 @@
-function createTextFile() {
-    const text = "Hi, how are you?"; // Content to write
-    const blob = new Blob([text], { type: 'text/plain' }); // Create a blob
-    const url = URL.createObjectURL(blob); // Create a URL for the blob
-
-    const a = document.createElement('a'); // Create a link element
-    a.href = url; // Set the href to the blob URL
-    a.download = 'test.txt'; // Set the filename
-    document.body.appendChild(a); // Append link to the document
-    a.click(); // Programmatically click the link to trigger download
-    document.body.removeChild(a); // Remove the link after download
-    URL.revokeObjectURL(url); // Release the blob URL
-}
-
-createTextFile();
+window.onload = function() {
+    alert('Hello!');
+};
